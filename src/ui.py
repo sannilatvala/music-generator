95567d62ebc36e659fbad7ee7977fb5cef554d6c
sannilatvala <119106675+sannilatvala@users.noreply.github.com>
2024-03-23 19:04:45 +0200

    index.py and ui.py created

---
+++ src/ui.py
@@ -0,0 +1,4 @@
+def get_user_input():
+    notes = input("Enter the input sequence of notes separated by spaces: ")
+    length = int(input("Enter the length of the generated music sequence: "))
+    return notes, length
