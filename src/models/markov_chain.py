1da6709e14ecbdc86997eb8048c6e10c9fb0f5d6
sannilatvala <119106675+sannilatvala@users.noreply.github.com>
2024-04-03 16:11:46 +0300

    fixed the issues in the Markov chain algorithm

--- src/models/markov_chain.py
+++ src/models/markov_chain.py
@@ -1,5 +1,6 @@
-from models.trie import Trie
 import random
+from models.trie import Trie
+
 
 class MarkovChain:
     """
@@ -11,9 +12,9 @@ class MarkovChain:
         self.notes = notes
         self.trie = Trie()
 
-    def train(self):
-        for i in range(len(self.notes)):
-            sequence = self.notes[i:]
+    def create_model(self, ngram=1):
+        for i in range(len(self.notes)-ngram):
+            sequence = self.notes[i:i+ngram+1]
             self.trie.insert_notes(sequence)
 
     def generate_sequence(self, length):
@@ -22,11 +23,18 @@ class MarkovChain:
 
         for _ in range(length - 1):
             if current_node.end_of_sequence:
-                current_node = self.trie.root.children[current_node.note]
+                # if current node is not a child of the root (i.e, it is the last note of the
+                # sequence without chilren) choose a random node from the children of the root
+                if current_node.note in self.trie.root.children:
+                    current_node = self.trie.root.children[current_node.note]
+                else:
+                    current_node = random.choice(
+                        list(self.trie.root.children.values()))
+                    sequence.append(current_node.note)
+                    continue
 
-            transition_counts = [node.transition_counts for node in current_node.children.values()]
-
-            print(transition_counts)
+            transition_counts = [
+                node.transition_counts for node in current_node.children.values()]
 
             next_node = random.choices(
                 list(current_node.children.values()),
